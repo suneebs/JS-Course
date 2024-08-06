01b775be6a16e1f6051efa8db8e07c33c2b53efc
suneeb <ssuneebvishnu@gmail.com>
2024-08-07 00:02:21 +0530

    Reveal elements on scroll

--- Bankist-website/script.js
+++ Bankist-website/script.js
@@ -137,4 +137,21 @@ const headerObserver = new IntersectionObserver(stickyNav,{
   threshold:0,
   rootMargin: `-${navHeight}px`,
 })
-headerObserver.observe(header)
+headerObserver.observe(header);
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+const allSections = document.querySelectorAll('.section');
+const revealSection = function(entries,observer){
+  const [entry] = entries;
+  if(!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target)
+}
+const sectionObserver = new IntersectionObserver(revealSection,{
+root:null,
+threshold:0.15,
+});
+allSections.forEach(function(section){
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+})
