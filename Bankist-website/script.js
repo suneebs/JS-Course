e36c44a3cec8d4c6f01a3bdbdf01db07aa61947b
suneeb <ssuneebvishnu@gmail.com>
2024-08-06 22:52:39 +0530

    Sticky navigation bar

--- Bankist-website/script.js
+++ Bankist-website/script.js
@@ -118,3 +118,23 @@ const handleHover=function(e){
 
 nav.addEventListener('mouseover',handleHover.bind(0.5));
 nav.addEventListener('mouseout',handleHover.bind(1));
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Sticky Nav
+
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function(entries){
+  const [entry] = entries
+  console.log(entry);
+  
+  if(!entry.isIntersecting) nav.classList.add('sticky')
+    else nav.classList.remove('sticky')
+}
+
+const headerObserver = new IntersectionObserver(stickyNav,{
+  root:null,
+  threshold:0,
+  rootMargin: `-${navHeight}px`,
+})
+headerObserver.observe(header)
