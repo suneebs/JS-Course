d8fe88ee80d5968ca1e08e1ab31e71bf58e3ec08
suneeb <ssuneebvishnu@gmail.com>
2024-08-10 12:18:13 +0530

    slider: click dot function

--- Bankist-website/script.js
+++ Bankist-website/script.js
@@ -182,19 +182,33 @@ imgTargets.forEach(img => imgObserver.observe(img));
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 //Slider component
-
+const slider = function(){
 const slides = document.querySelectorAll('.slide');
 const btnLeft = document.querySelector('.slider__btn--left');
 const btnRight = document.querySelector('.slider__btn--right');
+const dotContainer = document.querySelector('.dots');
 let curSlide = 0;
 const maxSlide = slides.length;
+
+const createDots = function(){
+  slides.forEach( function( _ ,i){
+    dotContainer.insertAdjacentHTML(
+      'beforeend',
+      `<button class="dots__dot" data-slide="${i}"></button>`
+    );
+  });
+};
+
+const activeDot = function(slide){
+  document.querySelectorAll('.dots__dot').forEach( dot => dot.classList.remove('dots__dot--active'));
+  document.querySelector(`.dots__dot[data-slide="${slide}"]`).classList.add('dots__dot--active');
+}
 
 const goToSlide = function(slide){
   slides.forEach((s,i) => {
     s.style.transform = `translateX(${100 * (i - slide)}%)`
   })
 }
-goToSlide(0);
 
 const nextSlide = function(){
   if(curSlide === maxSlide-1)
@@ -203,6 +217,7 @@ const nextSlide = function(){
    curSlide++
 
   goToSlide(curSlide);
+  activeDot(curSlide);
 }
 
 const prevSlide = function(){
@@ -212,7 +227,16 @@ const prevSlide = function(){
    curSlide--;
 
    goToSlide(curSlide);
+   activeDot(curSlide);
 }
+
+//iniialization
+const init =function(){
+  goToSlide(0);
+  createDots();
+  activeDot(0);
+}
+init()
 btnRight.addEventListener('click',nextSlide);
 btnLeft.addEventListener('click',prevSlide);
 
@@ -221,3 +245,14 @@ document.addEventListener('keydown', function(e){
   if(e.key === "ArrowRight") nextSlide();
   e.key === "ArrowLeft" && prevSlide();
 })
+
+document.addEventListener('click',function(e){
+  if(e.target.classList.contains('dots__dot')){
+    const {slide} = e.target.dataset;
+    goToSlide(slide);
+    activeDot(slide);
+  }
+    
+})
+}
+slider();
