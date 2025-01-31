1a7a988ac5b2b178e6b7156ff72239f46e9e6d8f
suneeb <ssuneebvishnu@gmail.com>
2025-01-31 14:00:44 +0530

    implement API configuration and JSON fetching with timeout handling

--- forkify/src/js/model.js
+++ forkify/src/js/model.js
@@ -1,4 +1,6 @@
 import { async } from 'regenerator-runtime';
+import { API_URL } from './config';
+import { getJSON } from './helpers';
 
 export const state ={
     recipe:{},
@@ -6,11 +8,8 @@ export const state ={
 
 export const loadRecipe = async function(id){
     try{
-
-    
-        const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`);
-        const data = await res.json();
-        if(!res.ok) throw new Error(`${data.message} (${res.status})`);
+        const data = await getJSON(`${API_URL}${id}`);
+        
         const {recipe} = data.data;
         state.recipe = {
             id: recipe.id,
@@ -24,6 +23,7 @@ export const loadRecipe = async function(id){
         };
         console.log(state.recipe);
     }catch(err){
-        alert(err);
+        // Temp error handling
+        console.error(`${err} 💥💥💥`);
     }
 }
