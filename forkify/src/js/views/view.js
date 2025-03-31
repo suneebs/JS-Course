2d483062bd536558c198a36c4ad0c092ecba9d74
suneeb <ssuneebvishnu@gmail.com>
2025-03-31 20:51:51 +0530

    feat: add JSDoc comments to render method for improved documentation

--- forkify/src/js/views/view.js
+++ forkify/src/js/views/view.js
@@ -2,6 +2,17 @@ import icons from 'url:../../img/icons.svg'; // Parcel 2
 
 export default class view{
     _data;
+
+    /**
+     * Render the received object to the DOM
+     * @param {Object | Object[]} data The data to be rendered(eg. recipe)
+     * @param {boolean} [render=true] if false, create markup string instead of rendering to the DOM.
+     * @returns {undefined | string } A markup string is returned if render = false
+     * @this {Object} View instance
+     * @author Suneeb S
+     * @todo Finish Implementation
+     */
+
         render(data, render = true){
             if(!data || (Array.isArray(data) && data.length === 0)) return this.renderError();
             this._data = data;
